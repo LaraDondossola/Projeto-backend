2c631a3f3fdc23af6c5ee5cb07d2531da5b67882
ana <124842977+anajulidorio@users.noreply.github.com>
2025-10-24 23:52:47 -0300

    Delete src directory

--- src/main/java/com/example/cuidar/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cuidar.repositories;
-
-import com.example.cuidar.models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByLogin(String login);
-}
