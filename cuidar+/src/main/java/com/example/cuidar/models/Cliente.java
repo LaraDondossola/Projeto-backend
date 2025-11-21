dbccbc681a78b4cea73929d8dbd90422a24add2a
ana <124842977+anajulidorio@users.noreply.github.com>
2025-11-21 10:49:51 -0300

    Update Cliente.java

--- cuidar+/src/main/java/com/example/cuidar/models/Cliente.java
+++ cuidar+/src/main/java/com/example/cuidar/models/Cliente.java
@@ -6,26 +6,19 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_clientes")
 public class Cliente extends Usuario {
-
-    // O 'id', 'login', 'senha', 'role' e 'ativo' são herdados de Usuario.
-    // REMOVEMOS OS CAMPOS DUPLICADOS DAQUI.
-
-    // Campos que são específicos de Cliente
     private String nome;
     private String telefone;
 
     public Cliente() {
-        super(); // Chama o construtor do Usuario
+        super();
     }
 
-    // Construtor opcional para facilitar
     public Cliente(String nome, String telefone) {
         super();
         this.nome = nome;
         this.telefone = telefone;
     }
 
-    // Getters e Setters para os campos de Cliente
     public String getNome() {
         return nome;
     }
