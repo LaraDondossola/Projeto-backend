7f3256e6a69746e9ff27dbbfc4d091e1d39488bf
ana <124842977+anajulidorio@users.noreply.github.com>
2025-11-21 10:53:52 -0300

    Update CustomAccessDeniedHandler.java

--- cuidar+/src/main/java/com/example/cuidar/security/config/CustomAccessDeniedHandler.java
+++ cuidar+/src/main/java/com/example/cuidar/security/config/CustomAccessDeniedHandler.java
@@ -14,11 +14,8 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        // Define o status da resposta como 403 Forbidden
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        // Define o tipo de conteúdo da resposta
         response.setContentType("application/json");
-        // Escreve o corpo da resposta
         response.getWriter().write("{\"error\": \"Acesso Negado\", \"message\": \"Você não tem permissão para acessar este recurso.\"}");
     }
 }
